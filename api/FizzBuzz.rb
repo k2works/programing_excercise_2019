4c8a1f60d3f2e65dea54201ccb265b69cbf4c5e3
k2works <kakimomokuri@gmail.com>
2019-12-27 19:28:01 +0900

    test: 2を渡したら文字列2を返す

--- api/FizzBuzz.rb
+++ api/FizzBuzz.rb
@@ -4,6 +4,7 @@ require 'minitest/autorun'
 =begin
 - [ ] 数を文字列にして返す
   - [x] 1を渡したら文字列"1"を返す
+  - [x] 2を渡したら文字列"2"を返す
 - [ ] 3 の倍数のときは数の代わりに｢Fizz｣と返す
 - [ ] 5 の倍数のときは｢Buzz｣と返す
 - [ ] 3 と 5 両方の倍数の場合には｢FizzBuzz｣と返す
@@ -17,10 +18,17 @@ class FizzBuzzTest < Minitest::Test
         # 検証
         assert_equal "1", FizzBuzz.generate(1)
     end
+
+    def test_2を渡したら文字列2を返す
+        # 前準備
+        # 実行
+        # 検証
+        assert_equal "2", FizzBuzz.generate(2)
+    end
 end
 
 class FizzBuzz
     def self.generate(n)
-        "1"
+        n.to_s
     end
 end
