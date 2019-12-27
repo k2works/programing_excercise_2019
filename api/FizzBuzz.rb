2f5115180de8a527123d3809066e724f8d5f5dd2
k2works <kakimomokuri@gmail.com>
2019-12-27 19:42:37 +0900

    refactor: メソッドの変更

--- api/FizzBuzz.rb
+++ api/FizzBuzz.rb
@@ -33,7 +33,7 @@ end
 class FizzBuzz
     def self.generate(number)
         result = number.to_s
-        if number % 3 == 0
+        if number.modulo(3) == 0
             result = "Fizz"
         end
         result
